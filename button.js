68d92a2d5371520cb047e3405ca3ab5ec846dc60
johnsonkohler <ozziedots@icloud.com>
2025-08-20 21:16:28 -0400

    Update button.js

--- button.js
+++ button.js
@@ -1,5 +1,3 @@
-const tester = 3;
-
 function gogo() {
   window.location.href = 'https://ia801509.us.archive.org/10/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4';
 }
