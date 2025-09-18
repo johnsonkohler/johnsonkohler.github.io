9f5adb02e67edc56bfc6e43c0352bf4d2c8dbe89
johnsonkohler <ozziedots@icloud.com>
2025-09-18 12:42:38 -0400

    Update button.js

--- button.js
+++ button.js
@@ -1,3 +1,43 @@
-function gogo() {
-  window.location.href = 'https://ia801509.us.archive.org/10/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4';
-}
+//global vars
+
+var lang; //equals 0 (FR) or 1 (EN)
+var prompt;
+var question;
+var responses = [];
+
+const promptsByLang = [promptsFR, promptsEN];
+const qsByLang = [qsFR, qsEN];
+const wordlistsByLang = [wordsFR, wordsEN];
+
+const promptsFR = []; //French prompts like "Next", "Finish", etc.
+const promptsEN = []; //English versions of the above
+
+const qsFR = []; //Questionnaire text in French
+const qsEN = []; //Questionnaire text in English
+
+const wordlistEN = []; //English wordlist
+const wordlistFR = []; //French wordlist
+
+const narrative = `Words` //French narrative
+
+//--------------------------- on-load tasks:
+
+var r = document.querySelector(':root');
+
+//function for doubling and randomizing the wordlist goes here
+//call said function here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
