e6149005e17c995fc35c3bd505c46ee5e63d9067
johnsonkohler <ozziedots@icloud.com>
2025-09-19 00:33:43 -0400

    Update button.js

--- button.js
+++ button.js
@@ -3,7 +3,7 @@
 var lang = 2; //equals 0 (FR) or 1 (EN) or 2 (undefined)
 var prompt;
 var question;
-var responses = []; 
+var answers = []; 
 
 var listLang; 
 var listIndex = -1;
@@ -11,14 +11,19 @@ var listsRead = 0;
 
 var qIndex = -1;
 
-const promptsFR = [`Suivant`, `Début de la liste de mots`, `Fin de la liste de mots. Prenez un pause!`, `Continuer au questionnaire`]; //French prompts
-const promptsEN = [`Next`, `Beginning of word list`, `End of word list. Take a break!`, `Continue to Questionnaire`]; //English prompts
+const promptsFR = [`Suivant`, `Début de la liste de mots`, `Fin de la liste de mots. Prenez un pause!`, `Continuer au questionnaire`, `Terminer`]; //French prompts
+const promptsEN = [`Next`, `Beginning of word list`, `End of word list. Take a break!`, `Continue to Questionnaire`, `Finish`]; //English prompts
 
-const qsFR = []; //Questionnaire text in French
-const qsEN = []; //Questionnaire text in English
-
-const wordlistEN = [`word 1`, `word 2`, `word 3`]; //English wordlist
 const wordlistFR = [`mot 1`, `mot 2`, `mot 3`]; //French wordlist
+const wordlistEN = [`word 1`, `word 2`, `context <br> <br> word 3`]; //English wordlist
+
+//Questionnaire text in FR then EN
+const qsFR = [["Directives pour le questionnaire", "Les douze prochaines questions portent sur vous (votre genre, votre sexe, les langues que vous parlez, votre identité culturelle, etc.). Veuillez répondre à chaque question aussi complètement et justement que vous le voulez selon votre aise (souvenez-vous que vos réponses demeureront anonymes). Si vous ne voulez pas répondre à une question, vous pouvez laisser le champ vide. Notez qu’une fois que votre réponse sera soumise, vous ne pourrez plus la changer : il n’est pas possible de revenir aux questions précédentes. Sélectionnez « Suivant » quand vous êtes prêt·e·s !"], ["Quel est votre genre ?", "Veuillez répondre le plus complètement et justement que vous le voulez selon votre aise."], ["Quel est votre genre ?", "(Si vous avez répondu à la question précédente en français, veuillez répondre à celle-ci en anglais, et vice-versa)"], ["À quel point vous sentez-vous ___ aujourd’hui, ou au moment où vous avez effectué cet enregistrement ?", "", "Féminin·e", "", "", "Masculin·e"], ["Vous identifiez-vous comme faisant partie de la communauté LGBTQ+ ?", "Veuillez répondre le plus complètement et justement que vous le voulez selon votre aise.", "", "Diriez-vous que plusieurs de vos ami·e·s s’identifient comme faisant partie de la communauté LGBTQ+ ?", "Veuillez répondre le plus complètement et justement que vous le voulez selon votre aise.", ""], ["Quel âge avez-vous (en années) ?", "", "", "Quel est votre lieu de naissance ?", "", ""], ["Comment décririez-vous votre niveau de compétence en :", "", "français", "", "", "anglais"], ["À quel âge avez-vous acquis :", "(0 = votre langue maternelle ; vous pouvez sélectionner 0 pour plus d’une langue)", "français", "", "", "anglais"], ["Quelle(s) langue(s) parlez-vous à la maison ?", "", "", "Parlez-vous une ou des langues autre(s) que le français et l’anglais ? Si oui, à quel niveau de compétence ?"], ["Merci !", "Merci d’avoir participé à cette étude ! Vous pouvez dorénavant redonner cet appareil à la personne qui vous l’a confié. Si vous avez des questions, n’hésitez pas à nous les demander !"], ["Données:"]]; 
+const qsEN = [["Questionnaire Instructions", "The following 12 questions are about you (your gender, your sex, the languages you speak, your cultural identity, etc.). Please answer each question as completely and accurately as you are comfortable (remember your responses will stay anonymous). If you don’t want to answer a question at all, you can leave it blank. Note that once you move on from a question, you can’t change your answer; there’s no Back button. Click ‘Next’ whenever you’re ready!"], ["What is your gender?", "Please answer as accurately and completely as you feel comfortable."], ["What is your gender?", "(If you answered the previous question in English, please answer this one in French, and vice versa)"], ["How ___ are you feeling today, or when you made this recording?", "", "Feminine", "", "", "Masculine"], ["Do you identify as a member of the LGBTQ community?", "Please answer as accurately and completely as you feel comfortable.", "", "Would you say that many of your friends identify as LGBTQ?", "Please answer as accurately and completely as you feel comfortable.", ""], ["How old are you (in years)?", "", "", "What is your place of birth?", "", ""], ["How would you rate your proficiency in:", "", "French", "", "", "English"], ["At what age did you acquire:", "(0 = mother tongue; this can be true for both languages.)", "French", "", "", "English"], ["What languages do you speak at home?", "", "", "Do you speak any other languages? If so, how well?"], ["Thank you!", "Thank you for participating in this study! Hand this device back to the person who gave it to you. If you have any questions, feel free to ask!"], ["Responses:"]]; 
+
+//DOI elements and the formats they should take during the questionnaire section
+const space = ["p1", "little-p1", "label1", "p2", "little-p2", "label2"];
+const formats = [["hidden", "hidden"], ["text", "hidden"], ["text", "hidden"], ["range", "range"], ["text", "text"], ["number", "text"], ["range", "range"], ["number", "number"], ["text", "text"], ["hidden", "hidden"], ["text", "hidden"]];
 
 const promptsByLang = [promptsFR, promptsEN];
 const qsByLang = [qsFR, qsEN];
@@ -26,8 +31,14 @@ const list = [wordlistFR, wordlistEN];
 
 const narrative = `Beaucoup de mots`; //French narrative
 
-const qbutton = document.getElementById("qbutton");
 const p1 = document.getElementById("p1");
+const input1 = document.getElementById("input1");
+const label1 = document.getElementById("label1");
+const p2 = document.getElementById("p2");
+const input2 = document.getElementById("input2");
+const label2 = document.getElementById("label2");
+const qbutton = document.getElementById("qbutton");
+
 
 //---------------------------------------------------------------------------------- on-load tasks
 
@@ -149,10 +160,44 @@ function questionnaire() {
   p1.innerHTML = question[0];
 }
 
-function q() {
-  qIndex++;
 
+function q() {
+  if (qIndex > 1) {
+    answers += input1.value;
+    answers += "; ";
+    answers += input2.value;
+    answers += "; ";
+  }
+
+  input1.value = "";
+  input2.value = "";
+
+  input1.setAttribute("type", formats[qIndex][0]);
+  input2.setAttribute("type", formats[qIndex][1]);
+
+  if (input1.type == "range") {
+    input1.setAttribute("style", "height: 25px");
+    input2.setAttribute("style", "height: 25px");
+  } else {
+    input1.setAttribute("style", "height: 100px");
+    input2.setAttribute("style", "height: 100px");
+  }
+
+  for (i=0; i<6; i++) {
+    if (i < question[qIndex].length) {
+      document.getElementById(space[i]).innerHTML = question[qIndex][i];
+    } else {
+      document.getElementById(space[i]).innerHTML = " ";
+    }
+  }
   
+  qIndex++;
+  
+  if (qIndex == 9) {
+    qbutton.innerHTML = prompt[4];
+  }
+  
+  if (qIndex == 11) input1.value = answers;
 }
 
 
