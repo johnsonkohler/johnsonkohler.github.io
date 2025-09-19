0d01e001769862981d8974eaccd7132b0d91f3ec
johnsonkohler <ozziedots@icloud.com>
2025-09-18 23:02:29 -0400

    Update button.js

--- button.js
+++ button.js
@@ -95,7 +95,7 @@ function setLang(a) {
 function run() { 
   document.getElementById("startpage").remove();
 
-  alert(`JS updated 22:53`);
+  alert(`JS updated 23:02`);
 
   qbutton.innerHTML = prompt[0]; //Launch button --> Next button
   qbutton.setAttribute(`onclick`, `wordlist`);
